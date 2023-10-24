6ff5b011426d039c24f36bdf2805a06da129422a
Red-Fish <365795976@qq.com>
2023-10-24 16:20:36 +0800

    init

---
+++ SwiftUILearn/SwiftUILearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearnApp.swift
+//  SwiftUILearn
+//
+//  Created by Red-Fish on 2023/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
